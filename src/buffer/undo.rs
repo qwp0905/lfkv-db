d3d80cbceea597b09aeafb2b38098b4c8ae84ef5
qwp0905 <kwonjin@jentestore.com>
2024-01-29 13:47:41 +0900

    add undo log

--- src/buffer/undo.rs
+++ src/buffer/undo.rs
@@ -1,5 +1,15 @@
-use crate::disk::PageSeeker;
+use crate::{disk::PageSeeker, Page};
 
 pub struct UndoLog {
+  log_index: usize,
+  tx_id: usize,
+  data: Page,
+  undo_index: usize,
+}
+
+pub struct Rollback {
   disk: PageSeeker,
+}
+impl Rollback {
+  pub fn get(&self, log_index: usize) {}
 }
