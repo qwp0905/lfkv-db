4c5ed0b5da7a73680bbdbec5177e9f651528c233
qwp0905 <qwp0905@gmail.com>
2024-01-29 20:03:15 +0900

    add undo serialize

--- src/buffer/undo.rs
+++ src/buffer/undo.rs
@@ -1,4 +1,6 @@
-use crate::{disk::PageSeeker, Page};
+use crate::{disk::PageSeeker, Error, Page, Result, Serializable, PAGE_SIZE};
+
+const UNDO_PAGE_SIZE: usize = PAGE_SIZE + 24;
 
 pub struct UndoLog {
   log_index: usize,
@@ -7,9 +9,44 @@ pub struct UndoLog {
   undo_index: usize,
 }
 
+impl UndoLog {
+  pub fn new(log_index: usize, tx_id: usize, data: Page, undo_index: usize) -> Self {
+    Self {
+      log_index,
+      tx_id,
+      data,
+      undo_index,
+    }
+  }
+}
+impl Serializable<Error, UNDO_PAGE_SIZE> for UndoLog {
+  fn serialize(&self) -> core::result::Result<Page<UNDO_PAGE_SIZE>, Error> {
+    let mut page = Page::new();
+    let mut wt = page.writer();
+    wt.write(&self.log_index.to_be_bytes())?;
+    wt.write(&self.tx_id.to_be_bytes())?;
+    wt.write(&self.undo_index.to_be_bytes())?;
+    wt.write(self.data.as_ref())?;
+
+    Ok(page)
+  }
+  fn deserialize(value: &Page<UNDO_PAGE_SIZE>) -> core::result::Result<Self, Error> {
+    let mut sc = value.scanner();
+    let log_index = sc.read_usize()?;
+    let tx_id = sc.read_usize()?;
+    let undo_index = sc.read_usize()?;
+    let data = sc.read_n(PAGE_SIZE)?.into();
+
+    Ok(UndoLog::new(log_index, tx_id, data, undo_index))
+  }
+}
+
 pub struct Rollback {
-  disk: PageSeeker,
+  disk: PageSeeker<UNDO_PAGE_SIZE>,
 }
 impl Rollback {
-  pub fn get(&self, log_index: usize) {}
+  pub fn get(&self, log_index: usize) -> Result<Page> {
+    let undo: UndoLog = self.disk.read(log_index * UNDO_PAGE_SIZE)?.deserialize()?;
+    Ok(undo.data)
+  }
 }
