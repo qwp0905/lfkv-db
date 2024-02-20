b48bc5ac5c921c0d269f4b320410497dcbc2b6dc
qwp0905 <kwonjin@jentestore.com>
2024-02-20 10:02:40 +0900

    add background thread

--- src/thread/background.rs
+++ src/thread/background.rs
@@ -5,10 +5,9 @@ use std::{
 
 use crossbeam::channel::Sender;
 
-use crate::{logger, ContextReceiver, StoppableChannel, UnwrappedSender};
-// fn testest() {
-//   StoppableThread::new(String::from("sdf"), 190, |v: usize| v);
-// }
+use crate::{
+  logger, ContextReceiver, StoppableChannel, UnwrappedReceiver, UnwrappedSender,
+};
 
 pub enum BackgroundJob<T, R> {
   New(fn(T) -> R),
@@ -80,122 +79,63 @@ where
   }
 }
 
-// pub struct BackgroundThread<T, R, F>
-// where
-//   F: Fn(T) -> R + Clone + 'static,
-// {
-//   _type: BackgroundJob,
-//   channel: StoppableChannel<T, R>,
-//   thread: Option<JoinHandle<()>>,
-//   job: F,
-//   name: String,
-//   stack_size: usize,
-// }
+pub struct BackgroundThread<T, R> {
+  job: BackgroundJob<T, R>,
+  channel: StoppableChannel<T, R>,
+  name: String,
+  stack_size: usize,
+  thread: Option<JoinHandle<()>>,
+}
 
-// impl<T, R, F> BackgroundThread<T, R, F>
-// where
-//   T: Send + 'static,
-//   R: Send + 'static,
-//   F: Fn(T) -> R + Send + Clone + 'static,
-// {
-// fn new(name: String, stack_size: usize, _type: BackgroundType<T, R>, job: F) -> Self {
-//   let (channel, rx) = StoppableChannel::new();
-//   let builder = Builder::new().name(name.clone()).stack_size(stack_size);
-//   let cloned = job.clone();
-//   Self {
-//     _type,
-//     channel,
-//     thread: Some(thread),
-//     job,
-//     name,
-//     stack_size,
-//   }
-// }
-// }
+impl<T, R> BackgroundThread<T, R>
+where
+  T: Send + 'static,
+  R: Send + 'static,
+{
+  pub fn new(name: String, stack_size: usize, job: BackgroundJob<T, R>) -> Self {
+    let (channel, rx) = StoppableChannel::new();
+    let thread = job.to_thread(name.clone(), stack_size, rx);
+    Self {
+      job,
+      channel,
+      name,
+      stack_size,
+      thread: Some(thread),
+    }
+  }
 
-// struct StoppableThread<T, R> {
-//   channel: StoppableChannel<T, R>,
-//   thread: JoinHandle<()>,
-// }
-// impl<T, R> StoppableThread<T, R>
-// where
-//   T: Send + 'static,
-//   R: Send + 'static,
-// {
-//   fn new<F>(name: String, stack_size: usize, f: Box<F>) -> Self
-//   where
-//     F: Fn(T) -> R + Send + 'static + Clone,
-//   {
-//     let (channel, rx) = StoppableChannel::new();
-//     let thread = Builder::new()
-//       .name(name)
-//       .stack_size(stack_size)
-//       .spawn(move || {
-//         while let Ok(v) = rx.recv_new() {
-//           f(v);
-//         }
-//       })
-//       .unwrap();
-//     Self { channel, thread }
-//   }
+  fn ensure_thread(&mut self) {
+    if let Some(thread) = &self.thread {
+      if !thread.is_finished() {
+        return;
+      }
+    }
 
-//   fn done<F>(name: String, stack_size: usize, f: Box<F>) -> Self
-//   where
-//     F: Fn(T) -> R + Send + 'static + Clone,
-//   {
-//     let (channel, rx) = StoppableChannel::new();
-//     let thread = Builder::new()
-//       .name(name)
-//       .stack_size(stack_size)
-//       .spawn(move || {
-//         while let Ok((v, done)) = rx.recv_done() {
-//           let r = f(v);
-//           done.must_send(r);
-//         }
-//       })
-//       .unwrap();
-//     Self { channel, thread }
-//   }
-// }
-// // impl<T, R> StoppableThread<T, R> {
-// //   fn terminate(self) -> std::thread::Result<()> {
-// //     if !self.thread.is_finished() {
-// //       self.channel.terminate();
-// //     }
-// //     self.thread.join()
-// //   }
-// // }
+    let (channel, rx) = StoppableChannel::new();
+    let nt = self.job.to_thread(self.name.clone(), self.stack_size, rx);
+    self.channel = channel;
+    self.thread = Some(nt);
+  }
 
-// pub struct Background<T, R, F> {
-//   thread: Option<StoppableThread<T, R>>,
-//   name: String,
-//   stack_size: usize,
-//   job: Box<F>,
-// }
-// impl<T, R, F> Background<T, R, F>
-// where
-//   T: Send + 'static,
-//   R: Send + 'static,
-//   F: Fn(T) -> R + Send + 'static + Clone,
-// {
-//   pub fn new(name: String, stack_size: usize, f: F) -> Self {
-//     let job = Box::new(f);
-//     let thread = StoppableThread::new(name.clone(), stack_size, job.clone());
-//     Self {
-//       thread: Some(thread),
-//       name,
-//       stack_size,
-//       job,
-//     }
-//   }
+  pub fn send(&self, v: T) {
+    self.channel.send(v);
+  }
 
-//   pub fn send(&mut self, t: T) {
-//     if let Some(thread) = &mut self.thread {
-//       if thread.thread.is_finished() {
-//         *thread =
-//           StoppableThread::new(self.name.clone(), self.stack_size, self.job.clone());
-//       }
-//       thread.channel.send(t);
-//     }
-//   }
-// }
+  pub fn send_and_wait(&self, v: T) -> R {
+    self.channel.send_with_done(v).must_recv()
+  }
+}
+
+impl<T, R> Drop for BackgroundThread<T, R> {
+  fn drop(&mut self) {
+    if let Some(t) = self.thread.take() {
+      if !t.is_finished() {
+        self.channel.terminate()
+      }
+
+      if let Err(err) = t.join() {
+        logger::error(format!("error on thread {}\n{:?}", self.name, err));
+      }
+    }
+  }
+}
