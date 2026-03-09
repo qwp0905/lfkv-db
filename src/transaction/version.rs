8974f88ee46d4d9620e6174a1f9beefa484e1791
qwp0905 <qwp0905@gmail.com>
2026-03-09 11:24:20 +0900

    add skiplist to version visibility to remove rwlock

--- src/transaction/version.rs
+++ src/transaction/version.rs
@@ -1,72 +1,68 @@
 use std::{
   collections::BTreeSet,
-  sync::{
-    atomic::{AtomicUsize, Ordering},
-    RwLock,
-  },
+  panic::RefUnwindSafe,
+  sync::atomic::{AtomicUsize, Ordering},
 };
 
-use crate::utils::ShortenedRwLock;
+use crossbeam_skiplist::SkipSet;
 
 pub struct VersionVisibility {
-  inner: RwLock<VersionVisibilityInner>,
+  active: SkipSet<usize>,
+  aborted: SkipSet<usize>,
   last_tx_id: AtomicUsize,
 }
 impl VersionVisibility {
   pub fn new(aborted: BTreeSet<usize>, last_tx_id: usize) -> Self {
     Self {
-      inner: RwLock::new(VersionVisibilityInner {
-        active: Default::default(),
-        aborted,
-      }),
+      active: Default::default(),
+      aborted: SkipSet::from_iter(aborted),
       last_tx_id: AtomicUsize::new(last_tx_id),
     }
-  }
-
-  pub fn remove_aborted(&self, version: &usize) {
-    let mut inner = self.inner.wl();
-    inner.aborted = inner.aborted.split_off(version);
-  }
-
-  pub fn is_aborted(&self, tx_id: &usize) -> bool {
-    self.inner.rl().aborted.contains(tx_id)
-  }
-  pub fn min_version(&self) -> usize {
-    self
-      .inner
-      .rl()
-      .active
-      .first()
-      .map(|v| *v)
-      .unwrap_or_else(|| self.current_version())
-  }
-  pub fn deactive(&self, tx_id: &usize) {
-    self.inner.wl().active.remove(tx_id);
-  }
-  pub fn is_visible(&self, tx_id: &usize) -> bool {
-    let inner = self.inner.rl();
-    !inner.aborted.contains(tx_id) && !inner.active.contains(tx_id)
-  }
-  pub fn is_active(&self, tx_id: &usize) -> bool {
-    self.inner.rl().active.contains(tx_id)
-  }
-  pub fn move_to_abort(&self, tx_id: usize) {
-    let mut inner = self.inner.wl();
-    if inner.active.remove(&tx_id) {
-      inner.aborted.insert(tx_id);
-    }
-  }
-  pub fn new_transaction(&self) -> usize {
-    let tx_id = self.last_tx_id.fetch_add(1, Ordering::Release);
-    self.inner.wl().active.insert(tx_id);
-    tx_id
   }
   pub fn current_version(&self) -> usize {
     self.last_tx_id.load(Ordering::Acquire)
   }
+  pub fn min_version(&self) -> usize {
+    self
+      .active
+      .front()
+      .map(|v| *v)
+      .unwrap_or_else(|| self.current_version())
+  }
+  pub fn is_visible(&self, tx_id: &usize) -> bool {
+    !self.is_active(tx_id) && !self.is_aborted(tx_id)
+  }
+  pub fn remove_aborted(&self, &version: &usize) {
+    while let Some(entry) = self.aborted.front() {
+      if *entry.value() >= version {
+        break;
+      }
+      entry.remove();
+    }
+  }
+
+  pub fn is_active(&self, tx_id: &usize) -> bool {
+    self.active.contains(tx_id)
+  }
+  pub fn is_aborted(&self, tx_id: &usize) -> bool {
+    self.aborted.contains(tx_id)
+  }
+
+  pub fn deactive(&self, tx_id: &usize) {
+    self.active.remove(tx_id);
+  }
+
+  pub fn move_to_abort(&self, tx_id: usize) {
+    self.aborted.insert(tx_id);
+    self.deactive(&tx_id);
+  }
+  pub fn new_transaction(&self) -> usize {
+    let tx_id = self.last_tx_id.fetch_add(1, Ordering::Release);
+    self.active.insert(tx_id);
+    tx_id
+  }
 }
 
-struct VersionVisibilityInner {
-  active: BTreeSet<usize>,
-  aborted: BTreeSet<usize>,
-}
+unsafe impl Send for VersionVisibility {}
+unsafe impl Sync for VersionVisibility {}
+impl RefUnwindSafe for VersionVisibility {}
