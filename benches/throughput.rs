799ff6b86a182368cbe599896c5b08c8ffec9751
qwp0905 <qwp0905@gmail.com>
2026-03-12 00:16:34 +0900

    add sequential/concurrent get + random operations benchmark

--- benches/throughput.rs
+++ benches/throughput.rs
@@ -32,6 +32,37 @@ fn pre_write_keys<
   tx.commit().unwrap();
 }
 
+fn bench_sequential_get(c: &mut Criterion) {
+  const SIZE: usize = 1_000;
+  let keys = (0..SIZE)
+    .map(|i| format!("{i:06}").as_bytes().to_vec())
+    .collect::<Vec<_>>();
+
+  let dir = TempDir::new_in(".").expect("dir failed.");
+  pre_write_keys(dir.path(), keys.iter());
+
+  let engine = build(dir.path())
+    .group_commit_count(1)
+    .build()
+    .expect("bootstrap error");
+
+  let mut group = c.benchmark_group("sequential-get");
+  group
+    .sample_size(10)
+    .measurement_time(Duration::from_secs(30))
+    .throughput(Throughput::Elements(SIZE as u64))
+    .bench_function("bench", |b| {
+      b.iter(|| {
+        for i in 0..SIZE {
+          let mut tx = engine.new_transaction().expect("start error");
+          tx.get(&keys[i].clone()).expect("get error");
+          tx.commit().expect("commit error");
+        }
+      });
+    });
+  group.finish();
+}
+
 fn bench_sequential_insert(c: &mut Criterion) {
   const SIZE: usize = 1_000;
   let keys = (0..SIZE)
@@ -98,6 +129,55 @@ fn bench_sequential_update(c: &mut Criterion) {
       });
     });
   group.finish();
+}
+
+fn bench_concurrent_get(c: &mut Criterion) {
+  const SIZE: usize = 100_000;
+  const THREADS_COUNT: usize = 1_000;
+
+  let dir = TempDir::new_in(".").expect("dir failed.");
+  let keys = (0..SIZE)
+    .map(|i| format!("{i:06}").as_bytes().to_vec())
+    .collect::<Vec<_>>();
+
+  pre_write_keys(dir.path(), keys.iter());
+  let engine = Arc::new(build(dir.path()).build().expect("bootstrap error"));
+  let (tx, rx) = unbounded::<(Vec<u8>, Sender<()>)>();
+  let threads = (0..THREADS_COUNT)
+    .map(|_| {
+      let rx = rx.clone();
+      let e = engine.clone();
+      std::thread::spawn(move || {
+        while let Ok((k, done)) = rx.recv() {
+          let mut tx = e.new_transaction().expect("start error");
+          tx.get(&k).expect("get error");
+          tx.commit().expect("commit error");
+          done.send(()).unwrap();
+        }
+      })
+    })
+    .collect::<Vec<_>>();
+
+  let mut group = c.benchmark_group("concurrent-get");
+  group
+    .sample_size(10)
+    .measurement_time(Duration::from_secs(30))
+    .throughput(Throughput::Elements(SIZE as u64))
+    .bench_function("bench", |b| {
+      b.iter(|| {
+        let mut waiting = Vec::with_capacity(SIZE);
+        for i in 0..SIZE {
+          let (t, r) = unbounded();
+          tx.send((keys[i].clone(), t)).unwrap();
+          waiting.push(r);
+        }
+        waiting.into_iter().for_each(|r| r.recv().unwrap());
+      });
+    });
+  group.finish();
+
+  drop(tx);
+  threads.into_iter().for_each(|t| t.join().unwrap());
 }
 
 fn bench_concurrent_insert(c: &mut Criterion) {
@@ -199,11 +279,71 @@ fn bench_concurrent_update(c: &mut Criterion) {
   threads.into_iter().for_each(|t| t.join().unwrap());
 }
 
+fn random_operations(c: &mut Criterion) {
+  use rand::{seq::SliceRandom, Rng};
+  const SIZE: usize = 100_000;
+  const THREADS_COUNT: usize = 1_000;
+
+  let keys = (0..SIZE)
+    .map(|i| format!("{i:06}").as_bytes().to_vec())
+    .collect::<Vec<_>>();
+
+  let dir = TempDir::new_in(".").expect("dir failed.");
+  pre_write_keys(dir.path(), keys.iter().take(SIZE / 3));
+
+  let engine = Arc::new(build(dir.path()).build().expect("bootstrap error"));
+  let (tx, rx) = unbounded::<(Vec<u8>, u8, Sender<()>)>();
+  let threads = (0..THREADS_COUNT)
+    .map(|_| {
+      let rx = rx.clone();
+      let e = engine.clone();
+      std::thread::spawn(move || {
+        while let Ok((k, op, done)) = rx.recv() {
+          let mut tx = e.new_transaction().expect("start error");
+          match op {
+            0 => drop(tx.remove(&k).expect("remove error")),
+            1 => drop(tx.get(&k).expect("get error")),
+            _ => drop(tx.insert(k.clone(), k).expect("insert error")),
+          }
+          tx.commit().expect("commit error");
+          done.send(()).unwrap();
+        }
+      })
+    })
+    .collect::<Vec<_>>();
+  let mut rng = rand::thread_rng();
+
+  let mut group = c.benchmark_group("random-operations");
+  group
+    .sample_size(10)
+    .measurement_time(Duration::from_secs(60))
+    .throughput(Throughput::Elements(SIZE as u64))
+    .bench_function("bench", |b| {
+      b.iter(|| {
+        let mut waiting = Vec::with_capacity(SIZE);
+
+        for key in keys.choose_multiple(&mut rng, SIZE) {
+          let (t, r) = unbounded();
+          tx.send((key.clone(), rng.gen_range(0..3), t)).unwrap();
+          waiting.push(r);
+        }
+        waiting.into_iter().for_each(|r| r.recv().unwrap());
+      });
+    });
+  group.finish();
+
+  drop(tx);
+  threads.into_iter().for_each(|t| t.join().unwrap());
+}
+
 criterion_group!(
   benches,
   bench_sequential_insert,
   bench_concurrent_insert,
   bench_sequential_update,
   bench_concurrent_update,
+  bench_sequential_get,
+  bench_concurrent_get,
+  random_operations
 );
 criterion_main!(benches);
